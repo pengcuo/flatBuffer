5605502041b881d08c4c70e7ffd75ca1de73c85f
pengcuo <zerenpengcuo@bytedance.com>
2019-10-25 21:02:05 +0800

    a simple demo about flatbuffer.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # flatBuffer
 This is a demo about flatBuffer.
+
+script:
+flatc -c -o ./ ./Layer_Fb.fbs
---
+++ compile.sh
@@ -0,0 +1,1 @@
+g++ main.cpp -I ./include/flatbuffers -I./include -lflatbuffers -L. -o test-flatbuffers -O3
